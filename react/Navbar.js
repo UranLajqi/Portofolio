19bd24c578cad34a891487f4f75fbef44c85c3b9
UranLajqi <58478405+UranLajqi@users.noreply.github.com>
2022-01-30 07:01:53 +0100

    Update Navbar.js

--- react/Navbar.js
+++ react/Navbar.js
@@ -2,10 +2,10 @@ function Navbar() {
     return (
         <>
           <a href="#header">HOME</a>
-          <a href="#about">ABOUT</a>
-          <a href="#resume">RESUME</a>
-          <a href="#se_skillz">SKILLS</a>
-          <a href="#contact">CONTACT</a>
+          <a href="#skills">SKILLS</a>
+          <a href="#education">EDUCATION</a>
+          <a href="#works">WORKS</a>
+          <a href="#contactMe">CONTACT</a>
         </>
     );
   }
